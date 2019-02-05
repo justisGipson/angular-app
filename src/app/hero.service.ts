12e4d375833fe9f30b50d2e17a36414c069d5e60
justisGipson <justis710@gmail.com>
2019-02-05 09:07:46 -0500

    finished tour of heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -29,6 +29,18 @@ export class HeroService {
     );
   }
 
+  getHeroNo404<Data>(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/?id=${id}`;
+    return this.http.get<Hero[]>(url).pipe(
+      map(heroes => heroes[0]),
+      tap(h => {
+        const outcome =h ? `fetched` : `did not find`;
+        this.log(`${outcome} hero id=${id}`);
+      }),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
   getHero(id: number): Observable<Hero>{
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
@@ -58,7 +70,7 @@ export class HeroService {
     const id = typeof hero === 'number'? hero : hero.id;
     const url = `${this.heroesUrl}/${id}`;
     return this.http.delete<Hero>(url, httpOptions).pipe(
-      tap(_ => this.log(`deleted herp id=${id}`)),
+      tap(_ => this.log(`deleted hero id=${id}`)),
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
